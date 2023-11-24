53557cd50dae1df2d0037d82627567d74cd7b139
Lolabunny123 <Llolabriggs@gmail.com>
2023-11-24 23:35:20 +0100

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Emmanuella David-Briggs on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
