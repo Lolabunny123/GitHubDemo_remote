561b1c66b0f558ec1a1e38d1b10a20cb4f9ca657
Lolabunny123 <Llolabriggs@gmail.com>
2023-11-25 16:31:45 +0100

    Create secondView

---
+++ GitHubDemo/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  GitHubDemo
+//
+//  Created by Emmanuella David-Briggs on 25/11/2023.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
